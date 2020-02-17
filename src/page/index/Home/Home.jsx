95b00e69187d2b2fae574c9f4fb5759c0c0b20d3
yzheng21 <zhengyufan94@gmail.com>
2020-02-16 23:44:28 -0800

    complete the content list comp

--- src/page/index/Home/Home.jsx
+++ src/page/index/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header/Header.jsx';
 import Category from './Category/Category.jsx';
+import ContentList from './ContentList/ContentList.jsx';
 
 /**
  * @constructor <Home />
@@ -13,6 +14,7 @@ class Home extends Component {
             <div>
                 <Header />
                 <Category />
+                <ContentList />
             </div>
         );
     }
--- src/page/index/reducers/main.js
+++ src/page/index/reducers/main.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import tabReducer from './tabReducer.js';
 import categoryReducer from './categoryReducer';
+import contentListReducer from './contentListReducer';
 
 const reducers = combineReducers({
     tabReducer,
-    categoryReducer
+    categoryReducer,
+    contentListReducer
 });
 
 export default reducers;
